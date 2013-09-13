d4f15da7a7ab5f119d8f479c8497e5dc5de46e29
Jim Pease <jmpease@codeserv.us>
2013-09-13 01:03:08 -0400

    Failed to commit test utility.

---
+++ test/util.js
@@ -0,0 +1,68 @@
+define(function(require) {
+
+    'use strict';
+
+    var q = QUnit,
+        _ = require('underscore'),
+        cacheUtil = require('ev-script/util/cache'),
+        eventsUtil = require('ev-script/util/events'),
+        evSettings = require('ev-config'),
+        FormsAuth = require('ev-script/auth/forms/auth'),
+        BasicAuth = require('ev-script/auth/basic/auth'),
+        AppInfo = require('ev-script/models/app-info'),
+        defaults = {
+            // Callback called after config is setup
+            setupConfig: function() {},
+            // Callback called after auth is setup
+            setupAuth: function() {},
+            // Set to true to immediately authenticate
+            authenticate: true
+        };
+
+    return {
+        setupHelper: function(appId, options) {
+            return function() {
+                q.stop();
+                var settings = _.extend({}, defaults, options);
+                this.appId = appId;
+                this.config = _.extend({}, evSettings);
+                eventsUtil.initEvents(this.appId);
+                cacheUtil.setAppConfig(this.appId, this.config);
+                settings.setupConfig.call(this);
+                this.info = new AppInfo({}, {
+                    appId: this.appId
+                });
+                cacheUtil.setAppInfo(this.appId, this.info);
+                this.info.fetch({})
+                .always(_.bind(function() {
+                    this.auth = (this.config.authType && this.config.authType === 'forms') ? new FormsAuth(this.appId) : new BasicAuth(this.appId);
+                    cacheUtil.setAppAuth(this.appId, this.auth);
+                    settings.setupAuth.call(this);
+                    if (settings.authenticate && !this.auth.isAuthenticated()) {
+                        this.auth.login({
+                            username: evSettings.testUser,
+                            password: evSettings.testPass
+                        })
+                        .then(function() {
+                            q.start();
+                        });
+                    } else {
+                        q.start();
+                    }
+                }, this));
+            };
+        },
+        teardownHelper: function() {
+            return function() {
+                if (this.auth.isAuthenticated()) {
+                    q.stop();
+                    this.auth.logout()
+                    .always(function() {
+                        q.start();
+                    });
+                }
+            };
+        }
+    };
+
+});
