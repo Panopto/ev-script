08d86d370bbf652e6f0b39485097f7ce68afe447
Jim Pease <jmpease@codeserv.us>
2019-11-07 09:51:38 -0500

    Updated version to 2.1.9

--- dist/ev-script.js
+++ dist/ev-script.js
@@ -1,5 +1,5 @@
 /**
- * ev-script 2.1.8 2019-10-18
+ * ev-script 2.1.9 2019-11-07
  * Ensemble Video Chooser Library
  * https://github.com/ensembleVideo/ev-script
  * Copyright (c) 2019 Symphony Video, Inc.
@@ -22916,7 +22916,7 @@ define('ev-script/models/dropbox-settings',['backbone'], function(Backbone) {
         defaults: {
             type: 'dropbox',
             width: '848',
-            height: '495',
+            height: '620',
             search: ''
         }
     });
@@ -31177,8 +31177,7 @@ define('ev-script/util/size',['require','underscore'],function(require) {
 
     return {
         optionsSixteenByNine: ['1280x720', '1024x576', '848x480', '720x405', '640x360', '610x344', '560x315', '480x270', '400x225', '320x180', '240x135', '160x90'],
-        // optionsDropbox: ['1280x750', '1024x600', '848x495', '720x420', '640x374', '610x356', '560x330', '480x280', '400x234', '320x190', '240x140', '160x94'],
-        optionsDropbox: ['1280x550', '1024x550', '848x550', '720x750', '640x750', '610x750', '560x750', '480x750', '400x750'],//, '320x190', '240x140', '160x94'],
+        optionsDropbox: ['1280x620', '1024x620', '848x620', '720x820', '640x820', '610x820', '560x820', '480x820', '400x820'],
         getAvailableDimensions: function(type) {
             return type && type === 'dropbox' ?
                 this.optionsDropbox :
