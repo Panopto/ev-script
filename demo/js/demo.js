aa590c5f15f7ca82b4d48eba326a9150b661b94f
Jim Pease <jmpease@codeserv.us>
2019-11-12 09:22:00 -0500

    Updated demo w/ sample settings overrides, automatic expansion of pickers during tab changes, and added a field for each embed type.

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -4,7 +4,6 @@
     'use strict';
 
     var app = new EV.EnsembleApp(_.extend(evSettings, {
-        hidePickers: false,
         scrollHeight: 200,
         fitToParent: true,
         getLocaleCallback: function() { return 'en-US'; },
@@ -13,16 +12,25 @@
         i18nPath: '../../src/ev-script/i18n'
     }));
 
+    // Sample default overrides
+    // EV.DropboxSettings.prototype.defaults.width = '480';
+    // EV.DropboxSettings.prototype.defaults.height = '820';
+    // EV.DropboxSettings.prototype.defaults.embedtype = 'responsive';
+    // EV.PlaylistSettings.prototype.defaults.embedtype = 'responsive';
+    // EV.PlaylistSettings.prototype.defaults.forceembedtype = true;
+
     app.events.bind('fieldUpdated', function($field, value) {
-        var $embed = $('.embed');
+        var $embeds = $('.embed'),
+            type = $field[0].id,
+            $embed = $embeds.filter('.' + type);
         if (value) {
-            if ($field[0].id === 'video') {
+            if (type === 'video') {
                 app.handleEmbed($embed, new EV.VideoSettings(value));
-            } else if ($field[0].id === 'playlist') {
+            } else if (type === 'playlist') {
                 app.handleEmbed($embed, new EV.PlaylistSettings(value));
-            } else if ($field[0].id === 'dropbox') {
+            } else if (type === 'dropbox') {
                 app.handleEmbed($embed, new EV.DropboxSettings(value));
-            } else if ($field[0].id === 'quiz') {
+            } else if (type === 'quiz') {
                 app.handleEmbed($embed, new EV.QuizSettings(value));
             }
             console.log('Embed code: ' + app.getEmbedCode(value));
@@ -34,6 +42,10 @@
     $(document).ready(function() {
         var $container = $('.chooserContainer'),
             $tabs = $('#tabs'),
+            $videoWrap = $('#videoWrap'),
+            $playlistWrap = $('#playlistWrap'),
+            $quizWrap = $('#quizWrap'),
+            $dropboxWrap = $('#dropboxWrap'),
             playlistSelected = false,
             dropboxSelected = false,
             quizSelected = false,
@@ -43,7 +55,8 @@
             };
         $(window).resize(resize);
         app.done(function() {
-            app.handleField($('#videoWrap')[0], new EV.VideoSettings(), '#video');
+            app.handleField($videoWrap[0], new EV.VideoSettings(), '#video');
+            $('.action-choose', $videoWrap).click();
             $tabs.tabs({
                 heightStyle: 'fill',
                 create: function() {
@@ -51,18 +64,37 @@
                     $tabs.show();
                 },
                 activate: function(e, ui) {
+                    var $embeds = $('.embed');
+                    $embeds.hide();
+                    if (ui.newTab.index() === 0) {
+                        // Automatically open chooser "folder"
+                        $('.action-choose', $videoWrap).click();
+                        $embeds.filter('.video').show();
+                    }
                     // Initialize playlist content once (and only once) the playlist tab is selected
-                    if (!playlistSelected && ui.newTab.index() === 1) {
-                        playlistSelected = true;
-                        app.handleField($('#playlistWrap')[0], new EV.PlaylistSettings(), '#playlist');
+                    if (ui.newTab.index() === 1) {
+                        if (!playlistSelected) {
+                            playlistSelected = true;
+                            app.handleField($playlistWrap[0], new EV.PlaylistSettings(), '#playlist');
+                        }
+                        $('.action-choose', $playlistWrap).click();
+                        $embeds.filter('.playlist').show();
                     }
-                    if (!dropboxSelected && ui.newTab.index() === 2) {
-                        dropboxSelected = true;
-                        app.handleField($('#dropboxWrap')[0], new EV.DropboxSettings(), '#dropbox');
+                    if (ui.newTab.index() === 2) {
+                        if (!dropboxSelected) {
+                            dropboxSelected = true;
+                            app.handleField($dropboxWrap[0], new EV.DropboxSettings(), '#dropbox');
+                        }
+                        $('.action-choose', $dropboxWrap).click();
+                        $embeds.filter('.dropbox').show();
                     }
-                    if (!quizSelected && ui.newTab.index() === 3) {
-                        quizSelected = true;
-                        app.handleField($('#quizWrap')[0], new EV.QuizSettings(), '#quiz');
+                    if (ui.newTab.index() === 3) {
+                        if (!quizSelected) {
+                            quizSelected = true;
+                            app.handleField($quizWrap[0], new EV.QuizSettings(), '#quiz');
+                        }
+                        $('.action-choose', $quizWrap).click();
+                        $embeds.filter('.quiz').show();
                     }
                 }
             });
